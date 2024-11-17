2d7be0ac5498dd9564d48845c38a02ed98f6c519
sheriff <manuel.lopez.sheriff@gmail.com>
2024-11-17 20:00:30 +0100

    Add edges corners tabs to config stickers

--- lib/screens/sticker_letters_tab.dart
+++ lib/screens/sticker_letters_tab.dart
@@ -3,10 +3,39 @@ import 'package:flutter/material.dart';
 class StickerLettersTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Sticker Letters Content',
-        style: TextStyle(fontSize: 24),
+    return DefaultTabController(
+      length: 2, // Number of tabs
+      child: Scaffold(
+        body: Column(
+          children: [
+            // TabBar
+            TabBar(
+              tabs: [
+                Tab(text: 'Edges'),
+                Tab(text: 'Corners'),
+              ],
+            ),
+            // TabBarView
+            Expanded(
+              child: TabBarView(
+                children: [
+                  Center(
+                    child: Text(
+                      'Edges Content',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Corners Content',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
