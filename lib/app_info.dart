49e150dcc0e5bb91a1e7e3a8e6e073974dde5a5c
sheriff <manuel.lopez.sheriff@gmail.com>
2024-12-16 10:05:04 +0100

    Add app info

---
+++ lib/app_info.dart
@@ -0,0 +1,10 @@
+class AppInfo {
+  static const String version = '1.0';
+  static const String buildNumber = '1';
+  static const String releaseDate = '2024-12-16';
+
+  static const List<String> features = [
+    'First version of the app',
+    '3 screens: Config, Practice, Notes',
+  ];
+}
